76cb5883821161d7c266ee8789517cf6228bc2d5
kpoter <kpoter@naver.com>
2024-06-04 16:21:54 +0900

    테스트 커밋

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactㅁㄴㅇㄻㄴㅇㄹ
         </a>
       </header>
     </div>
